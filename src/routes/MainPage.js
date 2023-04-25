6e49662ea2e5cdbfcd88b6f11c709d72d891596a
utfw <hwan.c.0330@gmail.com>
2023-04-25 09:04:47 +0900

    alert size absolutely

--- src/routes/MainPage.js
+++ src/routes/MainPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import requests from 'api/requests'
 import Banner from 'components/Banner'
 import Row from 'components/Row'
@@ -23,29 +22,3 @@ function MainPage() {
 }
 
 export default MainPage
-=======
-import requests from 'api/requests'
-import Banner from 'components/Banner'
-import Row from 'components/Row'
-import { auth } from '../firebase'
-import React, { useState } from 'react'
-
-function MainPage() {
-
-  return (
-    <div>
-    <Banner />
-    <Row title="Nexflix Original" id="NO" fetchUrl={requests.fetchNetflixOriginals} />
-    <Row title="Trending Now" id="TN" fetchUrl={requests.fetchTrending} />
-    <Row title="Top rated" id="TP" fetchUrl={requests.fetchTopRated} />
-    <Row title="Animation Movies" id="AM" fetchUrl={requests.fetchAnimationMovies} />
-    <Row title="Famliy Movies" id="FM" fetchUrl={requests.fetchFamilyMovies} />
-    <Row title="Adventure Movies" id="ADM" fetchUrl={requests.fetchAdventureMovies} />
-    <Row title="Sci-fi Movies" id="SM" fetchUrl={requests.fetchScienceFictionMovies} />
-    <Row title="Action Movies" id="ACM" fetchUrl={requests.fetchAnimationMovies} />
-    </div>
-  )
-}
-
-export default MainPage
->>>>>>> 4361134578777098eac019daca82fa350d460e5e
